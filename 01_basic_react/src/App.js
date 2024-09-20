4250a81d36e58916f081ceff0038c34939acb28e
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-20 17:29:19 +0530

    Deleting and editing the elements

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1> Start with React |  Ayushman Rout  | Things i learned- about npm , React scripts (start, build , test, eject)  </h1>
+    
+  );
+}
+
+export default App;
