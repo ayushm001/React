068361c17480c882d4105e5a8e92c5ea95b9548a
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-25 22:15:06 +0530

    Update main.jsx
    Rendering component's ,  Types of Elements rendering with Vite

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -3,8 +3,48 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function DemoApp(){
+    return(
+        <div>
+            <h1> Hello Customer </h1>
+        </div>
+    )
+        
+}
+
+// Element 1
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href : 'https://google.com',
+//         target: '_blank'
+//     },
+
+//     children : 'click me to visit'
+// }
+
+//Element 2
+const AnotherElement  = (
+    <a href='https://google.com' target='_blank'> visit google </a>
+)
+
+
+//Element3
+const ReactElement = React.createElement(
+    'a',
+    {href : 'https://google.com', target: '_blank'},
+    'CLICK ME TO VISIT'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    // <App />
+    // DemoApp()
+
+    // <DemoApp />
+    // ReactElement()
+    // AnotherElement()
+
+    ReactElement
+  
 )
