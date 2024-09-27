302e098cbea5238d01259b71132d0269b6984989
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-28 03:04:42 +0530

    using hook ( {useState})  , counter display page

---
+++ counter_02/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'         // hook calling
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // Hook's uses
+
+  const [dIsply_value , setCounter] = useState(10)
+
+  //let dIsply_value = 10  this variable is never gonna be reflect in UI section , it only reflect in backend (console) section 
+
+  const addVALUE = ()=>{
+    // console.log('clicked', dIsply_value);
+    // dIsply_value = dIsply_value + 1
+
+    setCounter(dIsply_value + 1) 
+  }
+
+  const removeVALUE = ()=> {
+    setCounter(dIsply_value - 1)
+  }
+
+
+  return (
+    <>
+     
+     <h1> React </h1>
+     <h2> Counter value {dIsply_value}</h2>
+     <button onClick={addVALUE}> Add Valu  {dIsply_value}</button>
+     <br />
+     <button onClick={removeVALUE}> Remove Value  {dIsply_value} </button>
+    </>
+  )
+}
+
+export default App
