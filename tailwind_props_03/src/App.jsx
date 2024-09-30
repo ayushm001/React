a29bc393101f091a185f798a3b82674f4d3a0ed2
Ayushman Rout <161948217+ayushm001@users.noreply.github.com>
2024-09-30 20:54:28 +0530

    installation  of Tailwind
    installation, Working etc

---
+++ tailwind_props_03/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <h1 className='bg-violet-600 text-black p-5 rounded-2xl' >  Let's start Tailwind </h1>
+
+     <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://www.bing.com/th?id=OIP.jQvFuRlmVesA7K6ArjfyrAHaH9&w=150&h=161&c=8&rs=1&qlt=90&o=6&cb=13&pid=3.1&rm=2" alt="" width="384" height="512"></img>
+  <div class="pt-6 text-center space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
+
+    </>
+  )
+}
+
+export default App
