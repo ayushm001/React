1f440396bd667ed33df62bc90cd7ae35030e4388
Ayushman Rout <ayushmanroutt01@gmail.com>
2024-10-30 12:19:31 +0530

    initiated react project of password mng

---
+++ PasswordGenerator_06/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <p className='bg-red-500'>hello chhchu gNDU</p>
+    </>
+  )
+}
+
+export default App
