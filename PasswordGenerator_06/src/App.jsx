e145db64a5fe877c99974c97ac0068c3a5db2513
Ayushman Rout <ayushmanroutt01@gmail.com>
2024-11-04 23:50:47 +0530

    Adding and Optimising the  Copy button

--- PasswordGenerator_06/src/App.jsx
+++ PasswordGenerator_06/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
   const [numberAllowed, setNumberAllowed]= useState(false);
   const[charAllowed, setcharAllowed] = useState(false);
   const[password, setPassword ]= useState(" ")
+
+  //  useRef HOOK
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -25,6 +28,12 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback( () => {
+    passwordRef.current?.select()
+    
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(()=>{
     passwordGenerator( )
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -43,9 +52,11 @@ function App() {
           className='outline-none w-full py-1 px-3'
           placeholder='password'
           readOnly
+          ref={passwordRef}
         />
 
-        <button className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'>
+        <button className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0' 
+        onClick={copyPasswordToClipboard}>
           Copy
         </button>
 
