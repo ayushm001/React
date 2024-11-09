8aab753ba463f9c57bdc6483382d0ade6cb1d51d
Ayushman Rout <ayushmanroutt01@gmail.com>
2024-11-09 20:44:36 +0530

    Update useCurrencyinfo.js

--- CurrencyConvertor_07/src/hooks/useCurrencyinfo.js
+++ CurrencyConvertor_07/src/hooks/useCurrencyinfo.js
@@ -1,13 +1,14 @@
-import { useEffect, useState, useStaten  } from "react";
+import { useEffect, useState  } from "react";
 
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch('url').then((res) => res.json()).then((res) => setData(res[currency]))
+        fetch('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_MMewTn64Gmeb5uVVZ7z1zvtoNiGIB46scFj2UTJ4')
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
-    console.log(data);
-    return data
+    return data   //  returns the data fetched and stored in the state
 }
 
-export default useCurrencyInfo   // custom hook design
+export default useCurrencyInfo   // custom hook, here it helps to return whole functionality's method  
